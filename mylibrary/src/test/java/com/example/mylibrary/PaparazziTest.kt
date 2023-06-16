5c3c6743192853a0f6a91e213357d4e0dcbe515e
nikki-red <eega.nikki@gmail.com>
2023-06-16 18:31:22 +0530

    Test cases added

--- mylibrary/src/test/java/com/example/mylibrary/PaparazziTest.kt
+++ mylibrary/src/test/java/com/example/mylibrary/PaparazziTest.kt
@@ -15,10 +15,16 @@ import android.view.LayoutInflater
 import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import app.cash.paparazzi.DeviceConfig
+import com.android.ide.common.rendering.api.SessionParams
 import com.example.teamslist_xml_paparazzi.MainActivity
+import com.example.mylibrary.RvAdapter
+import com.example.mylibrary.databinding.AppBarMainBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class PaparazziTest {
     @get:Rule
@@ -29,27 +35,44 @@ class PaparazziTest {
 
     @Test
     fun testEntireMusicApp() {
-        val view = paparazzi.inflate<ConstraintLayout>(R.layout.activity_main)
-        paparazzi.snapshot(view)
+        val binding = ActivityMainBinding.inflate(paparazzi.layoutInflater)
+        paparazzi.snapshot(binding.root)
     }
-/*
     @Test
     fun testToolbar() {
-        val view = paparazzi.inflate<ConstraintLayout>(R.layout.activity_main)
-        val toolbar = view.findViewById<Toolbar>(R.id.topBar)
-
-        paparazzi.snapshot(toolbar)
+        val binding = AppBarMainBinding.inflate(paparazzi.layoutInflater)
+        //val toolbar = binding.root.findViewById<MaterialToolbar>(R.id.topBar)
+        paparazzi.snapshot(binding.root)
     }
 
- */
-
+    private var groupList = ArrayList<Group>()
+    private lateinit var rvAdapter: RvAdapter
     @Test
     fun testRecyclerView() {
-        val view = paparazzi.inflate<ConstraintLayout>(R.layout.activity_main)
-        val recyclerView = view.findViewById<RecyclerView>(R.id.groupList)
+        val binding = ActivityMainBinding.inflate(paparazzi.layoutInflater)
+        val recyclerView = binding.groupList
 
-        paparazzi.snapshot(recyclerView)
+        rvAdapter = RvAdapter(groupList)
+        recyclerView.adapter = rvAdapter
+
+        val group1 = Group("Group 1", "Desc 1", false)
+        val group2 = Group("Group 2", "Desc 2", false)
+        val group3 = Group("Group 3", "Desc 3", false)
+        val group4 = Group("Group 4", "Desc 4", false)
+        val group5 = Group("Group 5", "Desc 5", false)
+        val group6 = Group("Group 6", "Desc 6", false)
+        val group7 = Group("Group 7", "Desc 7", false)
+
+        groupList.add(group1)
+        groupList.add(group2)
+        groupList.add(group3)
+        groupList.add(group4)
+        groupList.add(group5)
+        groupList.add(group6)
+        groupList.add(group7)
+
+        //rvAdapter.notifyDataSetChanged()
+
+        paparazzi.snapshot(binding.root)
     }
-
-
 }
